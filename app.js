8f9ce2488eb02af1cd47381266ae5696d0dbeb4e
Olivier DEWALQUE <olivier.dewalque@student.vinci.be>
2020-11-20 15:00:25 +0100

    Rajout de static

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(express.static(__dirname+'/public'));
 
-//app.use("/api/images", imagesRouter);
+app.use("/api/images", imagesRouter);
 
 
 module.exports = app;
