b3d7dd73e16e715722ce9672a2f779408db5749d
Olivier DEWALQUE <olivier.dewalque@student.vinci.be>
2020-11-23 14:27:52 +0100

    afficher param de game + lancer game

--- routes/games.js
+++ routes/games.js
@@ -3,11 +3,17 @@ let express = require("express");
 let router = express.Router();
 let Game = require("../models/Game.js");
 
+//Crée une nouvelle game
 router.post("/", function (req, res) {  
     let newGame = new Game(req.body);
     console.log("POST new Game : ", newGame);
     newGame.save();
     return res.json(newGame);
   });
+
+router.get("/",  function (req, res) { 
+  console.log("GET maxId : ",Game.maxId);
+  return res.json(Game.maxId);
+});
 
 module.exports = router;
