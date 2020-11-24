a842fbea2f546e35cb67d36a27730fa204d8457e
Christian GAVIRIA <christian.gaviria@student.vinci.be>
2020-11-24 16:03:00 +0100

    bug fix

--- app.js
+++ app.js
@@ -5,7 +5,27 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var gamesRouter = require("./routes/games");
 
-var app = express();
+
+let app = express();
+let myHttpExpressServer = require('http').createServer(app);
+
+
+const io = require("socket.io")(myHttpExpressServer, {
+    cors: {
+      origin: "http://localhost", // Client here is localhost:80
+      methods: ["GET", "POST"]
+    }
+  });
+   
+  io.on('connection', socket => {
+    console.log('New Socket Connection');
+    socket.emit("broadcast", "New Socket Client : Welcome !");
+  });
+   
+  myHttpExpressServer.listen(3000, ()  => {
+    console.log('Socket server listening on *:3000');
+  });
+
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -13,6 +33,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/api/games", gamesRouter);
+
 
 
 module.exports = app;
